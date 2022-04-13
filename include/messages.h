92094f692f416df035a2f1acd41b66d85fc0ebd3
Aleszp <aleksander.szsz@gmail.com>
2022-04-13 13:17:07 +0200

    Added extern C directives to headers for C++ compilers (just in case they would be needed).

--- include/messages.h
+++ include/messages.h
@@ -1,11 +1,18 @@
 #ifndef _MESSAGES_H_
 #define _MESSAGES_H_
 
+#ifdef __cplusplus
+extern "C" //make C++ compatible (just in case)
+{
+#endif
+
 void GPLnotice();
 void printHelp();
 void wrongUsage();
 void wrongFile(char* mode,char* path);
 
-
-
+#ifdef __cplusplus
+} //end of extern "C"
 #endif
+
+#endif	//end of _MESSAGES_H_
--- include/setup.h
+++ include/setup.h
@@ -1,8 +1,17 @@
 #ifndef _SETUP_H_
 #define _SETUP_H_
 
+#ifdef __cplusplus
+extern "C" //make C++ compatible (just in case)
+{
+#endif
+
 int handleStartup(int argc,char** argv,int* optind_,char* separator);
 int openFiles(int argc,char** argv,int optind,FILE** fileIn, FILE** fileOut);
 char selectSeparator(int argc,char** argv,int optind);
 
+#ifdef __cplusplus
+} //end of extern "C"
 #endif
+
+#endif	//_SETUP_H_
